9a4cb4cd5562833a0ac8d6935fafe34e3e94e15c
DoryanGrayBolado38 <lukinhasdeoliveiramoraes@gmail.com>
2024-11-14 22:36:51 -0300

    atualizacao

--- config/filesystems.php
+++ config/filesystems.php
@@ -27,20 +27,23 @@ return [
     | Supported drivers: "local", "ftp", "sftp", "s3"
     |
     */
-'disks' => [
-    'local' => [
-        'driver' => 'local',
-        'root' => storage_path('app'),
-    ],
-    'public' => [
-        'driver' => 'local',
-        'root' => storage_path('app/public'),
-        'url' => env('APP_URL') . '/storage',
-        'visibility' => 'public',
-    ],
-    // ...
 
+    'disks' => [
 
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app/private'),
+            'serve' => true,
+            'throw' => false,
+        ],
+
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
 
         's3' => [
             'driver' => 's3',
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PersonagensController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,20 +11,18 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('adm.gerenciamento');
 })->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/', [PersonagensController::class, "MostrarHome"])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::get('/home', [PersonagensController::class, "MostrarHome"])->name('home');
 Route::get('/cadastro-personagem', [PersonagensController::class, "MostrarCadastroPersonagem"])->name('cadastro-personagem');
 Route::post('/cadastrar-personagem', [PersonagensController::class, 'CadastroPersonagem'])->name('cadastrar-personagem');
 Route::get('/lista-personagem', [PersonagensController::class, "MostrarPersonagemNome"])->name('lista-personagem');
 Route::get('/altera-personagem/{id}', [PersonagensController::class, "MostrarPersonagemCodigo"])->name('altera-personagem');
 Route::delete('/apagar-personagem/{id}', [PersonagensController::class, 'Destroy'])->name('apagar-personagem');
 Route::put('/altera-personagem/{id}', [PersonagensController::class, 'Update'])->name('altera-personagem');
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-});
-
-
 
 require __DIR__.'/auth.php';
